307e4437665db61192490dbe80d4edaeb4f55607
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-09-19 11:48:01 +0900

    commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Layout from "../components/layout"
-import Hero from "../components/hero"
 import Seo from '../components/seo';
 
 const About = () => {
   return (
     <Layout>
-      <Hero />
       <Seo title="ABOUT" description="ProAca.inc about" />
     </Layout>
   )
